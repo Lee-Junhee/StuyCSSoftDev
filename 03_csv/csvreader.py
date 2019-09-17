46c4c4ffc3633422c1bc0d58d2e5748d39aec178
Lee-Junhee <junhee.lee002@gmail.com>
2019-09-17 01:50:56 +0000

    implemented dictionary making capabilities

---
+++ 03_csv/csvreader.py
@@ -0,0 +1,22 @@
+import random
+#just a string of numbers and other junk
+garbage = " 1234567890.,\n"
+
+#processing the csv file
+csv = open("occupations.csv", "r") #opens the file for reading
+occupations = csv.readlines(); #reads the lines into a list
+dictionary = {} #creates empty dictionary
+for job in occupations:
+    char = len(job) #sets length as variable
+    for num in range(1, char + 1):
+        #print(num) #verify that code is counting proper integers
+        if job[-num] == ",": #looks from the right to left for first comma
+            percent = job[char - num:]
+            #print("comma found") #verifies it finds commas
+            break
+    if percent.strip(garbage) == "": #if the percent is a valid float        
+        #add the key/value pair to dictionary
+        dictionary[job[:char-num].strip(' \"')] = float(percent.strip(",\n "))
+print(dictionary)
+
+#choosing weighted occupation
