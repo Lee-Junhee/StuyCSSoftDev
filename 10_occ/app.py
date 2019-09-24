fa6542c95aaaacff7d7510bde9a6b0adf0b2ab47
Lee-Junhee <junhee.lee002@gmail.com>
2019-09-23 22:17:57 -0400

    updated app

--- 10_occ/app.py
+++ 10_occ/app.py
@@ -17,29 +17,8 @@ def hello_world():
         </head>
         <body>
         <p>This was made using Flask</p>
-        <p><a href="./state">Subpage0</a></p>
-        <p><a href="./church">Subpage1</a></p>
-        <p><a href="./static/staticTest.html">static page test</a></p>
         </body>
     '''
-
-@app.route("/state")
-def preamb():
-    print(__name__) #prints "__main__"
-    return "We the People of the United States, in Order to form a more perfect Union, establish Justice, insure domestic Tranquility, provide for the common defence, promote the general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, do ordain and establish this Constitution for the United States of America."
-
-@app.route("/church")
-def gen1():
-    print(__name__) #prints "__main__"
-    return "In the beginning God created the heavens and the earth."
-
-coll = [0,1,1,2,3,5,8]
-@app.route("/my_foist_template")
-def disp():
-    print(__name__)
-    return render_template("seed.html",
-            foo="fooooo",
-            collection=coll)
 
 @app.route("/occupyflaskst")
 def occupations():
@@ -49,29 +28,26 @@ def occupations():
         for row in csvreader:
             rows.append(row)
 
-    dictionary = {}
+    occDict = {}
     for item in rows[1: len( rows) - 1]:
-        key = item[0]
-        value = float( item[1])
-        dictionary[ key] = value
-    chosen = random.random() * float(rows[len(rows) - 1][1])
-    #print(chosen) #verification step
-    for occupation in dictionary.keys():
-        chosen -= dictionary[occupation]
-        if chosen <= 0:
+        key = item[0] #key is the occupation name
+        occDict[key] = (float(item[1]),item[2]) #key is a tuple (percentage, link)
+    chosen = random.random() * float(rows[len(rows) - 1][1]) #random number [0.0, 1.0] multiplied by total
+
+    for occupation in occDict.keys():
+        chosen -= occDict[occupation][0] #keep subtracting the percentage of occupation
+        if chosen <= 0: #if the chosen number is less than or equal to zero, then we found the occupation
             random_occupation =  occupation   
             break
-    
-    occList=[]
-    for occ in dictionary.keys():
-        occList.append((occ, dictionary[occ]))
+        
 
     return render_template("table.html",
             title="Occupation Data",
+            header=("Junhee Lee, Michael Lin", "SoftDev1 pd1", "K10 -- Jinja Tuning", "2019-09-20"),
             heading="Occupations in the United States and Percentage of Workforce",
             occupation=random_occupation,
             tHead=['Occupation Title','Percentage of Workforce'],
-            entries=occList)
+            entries=occDict)
 
 if __name__ == "__main__":
     app.debug = True 
