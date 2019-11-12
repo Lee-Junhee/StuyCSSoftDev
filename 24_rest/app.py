cccd7f8289b8301d0fc3ee475ca961b0cdfc64e6
Lee-Junhee <jlee02@stuy.edu>
2019-11-12 08:37:54 -0500

    work from class on rest api use

---
+++ 24_rest/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from json import loads
+from urllib.request import urlopen
+app = Flask(__name__)
+
+@app.route("/")
+def renderAPI():
+    u = urlopen("https://api.nasa.gov/planetary/apod?api_key=6lP4sG1PtWFAweeObI0YVxwKHewiKiJFUGeSQptR")
+    response = u.read()
+    data = loads(response)
+    return render_template("root.html",
+            pic = data['hdurl'],
+            content = data['explanation'])
+
+if __name__ == "__main__":
+    app.debug = True 
+    app.run()
