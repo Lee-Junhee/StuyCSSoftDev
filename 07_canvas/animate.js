94bf8c870d660e84f260e59f3406ca90fff80621
Lee-Junhee <jlee02@stuy.edu>
2020-02-12 08:44:01 -0500

    pair programming

--- 07_canvas/animate.js
+++ 07_canvas/animate.js
@@ -6,30 +6,39 @@
  */
 
 
-//constant variable initalization
-const 
+//global variable initalization 
+var timestamp = 0;
 
-//canvas initialization
+
+//element initialization
 const c = document.getElementById("arena");
 const ctx = c.getContext("2d");
+const animate = document.getElementById("enable");
+const stop = document.getElementById("disable");
+const emergency = document.getElementById("estop");
 
 //animation code
+const enable = function() {
+	animate.removeEventListener("click", enable);
+	stop.addEventListener("click", disable);
+}
 
-
-
-
-
+const disable = function() {
+	stop.removeEventListener("click", disable);
+	animate.addEventListener("click", enable);
+}
 
 //estop code
-
-
-
-
+const estop = function() {
+	animate.removeEventListener("click", enable);
+	stop.removeEventListener("click", disable);
+}
 
 
 
 //button event handlers
-
+animate.addEventListener("click", enable);
+emergency.addEventListener("click", estop);
 
 
 
