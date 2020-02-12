34517b2646d816bc7916e8ea5b2e4fd7246b9f3f
Lee-Junhee <jlee02@stuy.edu>
2020-02-12 08:30:20 -0500

    added code for setting up canvas

--- 07_canvas/animate.js
+++ 07_canvas/animate.js
@@ -1,9 +1,17 @@
+/*
+ * PoA: Junhee Lee, Pratham Rawat
+ * SoftDev2 pd1
+ * K07 :: Animation
+ * 2020-02-12
+ */
+
+
 //constant variable initalization
+const 
 
-
-
-
-
+//canvas initialization
+const c = document.getElementById("arena");
+const ctx = c.getContext("2d");
 
 //animation code
 
