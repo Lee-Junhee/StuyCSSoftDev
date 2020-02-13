e122de2d026cdfcf6ea0331c31d049c22740065b
Lee-Junhee <jlee02@stuy.edu>
2020-02-13 08:22:21 -0500

    works now

--- 07_canvas-anim/animate.js
+++ 07_canvas-anim/animate.js
@@ -8,34 +8,53 @@
 
 //global variable initalization
 var timestamp = 0;
+const speed = 0.01;
+var currentFrame;
 
 
 //element initialization
 const c = document.getElementById("arena");
 const ctx = c.getContext("2d");
-const animate = document.getElementById("enable");
+ctx.fillStyle = "#0000ff";
+ctx.strokeStyle = "#000000";
+
+const start = document.getElementById("enable");
 const stop = document.getElementById("disable");
 const emergency = document.getElementById("estop");
 
 //animation code
+const animate = function() {
+	ctx.clearRect(0, 0, 600, 600);
+	ctx.beginPath();
+	ctx.arc(300, 300, 300 * (1 + Math.sin(Math.PI * timestamp * speed)) / 2, 0, Math.PI * 2);
+	ctx.fill();
+	ctx.stroke();
+	ctx.closePath();
+	timestamp += 1;
+	currentFrame = window.requestAnimationFrame(animate);
+}
+
 const enable = function() {
-	animate.removeEventListener("click", enable);
+	start.removeEventListener("click", enable);
 	stop.addEventListener("click", disable);
+	window.requestAnimationFrame(animate);
 }
 
 const disable = function() {
 	stop.removeEventListener("click", disable);
-	animate.addEventListener("click", enable);
+	start.addEventListener("click", enable);
+	window.cancelAnimationFrame(currentFrame);
 }
 
 //estop code
 const estop = function() {
-	animate.removeEventListener("click", enable);
+	disable();
+	start.removeEventListener("click", enable);
 	stop.removeEventListener("click", disable);
 }
 
 
 
 //button event handlers
-animate.addEventListener("click", enable);
+start.addEventListener("click", enable);
 emergency.addEventListener("click", estop);
