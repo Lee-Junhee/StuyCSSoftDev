61daba8e7f10ca9e1fdc723456bc17a57476c0dc
Lee-Junhee <junhee.lee002@gmail.com>
2020-02-13 19:32:14 -0500

    used drawImage to draw FIRST log

--- 08_canvas-anim/animate.js
+++ 08_canvas-anim/animate.js
@@ -8,7 +8,8 @@
 
 //global variable initalization
 var timestamp = 0;
-const speed = 0.5;
+const dspeed = 0.01;
+const pspeed = 1;
 var currentFrame;
 var projectile = [-1, -1, 1, 1];
 
@@ -24,14 +25,16 @@ const stop = document.getElementById("disable");
 const propulsion = document.getElementById("projectile");
 const emergency = document.getElementById("estop");
 
-//const logo = new Image();
-//logo.src = "";
+const logo = new Image();
+logo.src = "FIRST.jpg";
+const height = 27;
+const width = 105;
 
 //animation code
 const dot = function() {
 	ctx.clearRect(0, 0, 600, 600);
 	ctx.beginPath();
-	ctx.arc(300, 300, 300 * (1 + Math.sin(Math.PI * timestamp * speed)) / 2, 0, Math.PI * 2);
+	ctx.arc(300, 300, 300 * (1 + Math.sin(Math.PI * timestamp * dspeed)) / 2, 0, Math.PI * 2);
 	ctx.fill();
 	ctx.stroke();
 	ctx.closePath();
@@ -40,24 +43,24 @@ const dot = function() {
 }
 
 const propulse = function() {
-	projectile[0] += projectile[2] * speed;
-	projectile[1] += projectile[3] * speed;
+	projectile[0] += projectile[2] * pspeed;
+	projectile[1] += projectile[3] * pspeed;
 	console.log("P1" + projectile[0]);
 	console.log("P2" + projectile[1]);
 	ctx.clearRect(0, 0, 600, 600);
-	//ctx.drawImage();
-	ctx.beginPath();
-	ctx.arc(projectile[0], projectile[1], 20, 0, Math.PI * 2);
-	ctx.fill();
-	ctx.stroke();
-	ctx.closePath();
-	if (projectile[0] >= 600) {
-		projectile[2] = -1;
+	ctx.drawImage(logo, projectile[0], projectile[1], 70, 18);
+	//ctx.beginPath();
+	//ctx.arc(projectile[0], projectile[1], 20, 0, Math.PI * 2);
+	//ctx.fill();
+	//ctx.stroke();
+	//ctx.closePath();
+	if (projectile[0] >= 600 - width) {
+		projectile[2] = -2;
 	}
 	if (projectile[0] <= 0) {
-		projectile[2] = 1;
+		projectile[2] = 2;
 	}
-	if (projectile[1] >= 600) {
+	if (projectile[1] >= 600 - height) {
 		projectile[3] = -1;
 	}
 	if (projectile[1] <= 0) {
@@ -75,19 +78,19 @@ const auton = function() {
 }
 
 const teleop = function() {
-	start.addEventListener("click", enable);
+	start.addEventListener("click", auton);
 	stop.addEventListener("click", disable);
 	window.cancelAnimationFrame(currentFrame);
-	projectile[0] = Math.random() * 600;
-	projectile[1] = Math.random() * 600;
-	projectile[2] = 1;
+	projectile[0] = Math.random() * (600 - width);
+	projectile[1] = Math.random() * (600 - height);
+	projectile[2] = 2;
 	projectile[3] = -1;
 	window.requestAnimationFrame(propulse);
 }
 
 const disable = function() {
 	stop.removeEventListener("click", disable);
-	start.addEventListener("click", enable);
+	start.addEventListener("click", auton);
 	window.cancelAnimationFrame(currentFrame);
 }
 
