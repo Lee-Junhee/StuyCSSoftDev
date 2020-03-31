b797f07efb4302ba124f6701d70d8ba1d1fb1935
Lee-Junhee <junhee.lee002@gmail.com>
2020-03-31 07:48:22 -0400

    copied over partner work

--- 13_svg/svg.js
+++ 13_svg/svg.js
@@ -6,42 +6,44 @@
 //initialization
 const pic = document.getElementById("vimage");
 const button = document.getElementById("clear");
-const DOT_RADIUS = 10;
+const bbox = pic.getBoundingClientRect();
+const DOT_RADIUS = 25;
 const DOT_COLOR_0 = "black";
 const DOT_COLOR_1 = "red";
 
-/* var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-c.setAttribute("cx", 0);
-c.setAttribute("cy", 0);
-c.setAttribute("r", 100);
-c.setAttribute("fill", "red");
-c.setAttribute("stroke", "black");
-pic.appendChild(c); */
-
 const draw = function(e) {
-	if (e.target == pic){
-		let x = e.offsetX;
-		let y = e.offsetY;
-		let dot = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-		dot.setAttribute("cx", x);
-		dot.setAttribute("cy", y);
-		dot.setAttribute("r", DOT_RADIUS);
-		dot.setAttribute("fill", DOT_COLOR_0);
-		pic.appendChild(dot);
-		dot.addEventListener("mousedown", color);
-	}
+    if (e.target == pic) {
+        let x = e.offsetX;
+        let y = e.offsetY;
+        let dot = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+        dot.setAttribute("cx", x);
+        dot.setAttribute("cy", y);
+        dot.setAttribute("r", DOT_RADIUS);
+        dot.setAttribute("fill", DOT_COLOR_0);
+        dot.addEventListener("mousedown", color);
+        pic.appendChild(dot);
+    }
 };
 
-const color = function(e) {
+const color = function() {
 	this.removeEventListener("mousedown", color);
 	this.setAttribute("fill", DOT_COLOR_1);
 	this.addEventListener("mousedown", die);
-}
+};
 
-const die = function(e) {
-}
+const die = function() {
+    this.removeEventListener('mousedown', die);
+    this.setAttribute('fill', DOT_COLOR_0);
+    let x_range = bbox.width - 2 * DOT_RADIUS;
+    let y_range = bbox.height - 2 * DOT_RADIUS;
+    let x_offset = bbox.left + DOT_RADIUS;
+    let y_offset = bbox.top + DOT_RADIUS;
+    this.setAttribute('cx', Math.floor(Math.random() * x_range) + x_offset);
+    this.setAttribute('cy', Math.floor(Math.random() * y_range) + y_offset);
+    this.addEventListener('mousedown', color);
+};
 
-const clear = function(e) {
+const clear = function() {
 	pic.innerHTML = '';
 };
 
